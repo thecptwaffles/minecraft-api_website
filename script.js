034cc7fe88953324d0d8bd20b1b3bc53d0aeab94
Cade <thecptwaffles@zohomail.com>
2022-01-24 06:55:37 -0700

    gitting api to work

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 MinecraftAPI.getServerStatus('147.135.105.18', {
     port: 25567 // optional, only if you need a custom port
-}, function (err, status) {
+}, function (err, status, server, players) {
     if (err) {
-        return document.querySelector('.server-status').innerHTML = 'Error loading status';
+    return document.querySelector('.server-online').innerHTML = 'Error loading status';
     }
 
     // you can change these to your own message!
     document.querySelector('.server-online').innerHTML = status.online ? 'up' : 'down';
-});
+
+    document.querySelector('.server-motd').innerHTML = status.motd;
+
+    document.querySelector('.server-players').innerHTML = status.players.now;
+
+    document.querySelector('.server-max').innerHTML = status.players.max
+}
+);
+
